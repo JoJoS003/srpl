1d8ce21c3de4fe111dede32df48a4d6816d68f7b
JoJoS003 <jojos003@free.fr>
2013-02-04 15:01:31 +0100

    Add name and title properties to League. Change the League#round method to accept played match bu not in existing rounds.
    Add somes comments.

--- lib/srpl/league.rb
+++ lib/srpl/league.rb
@@ -5,54 +5,55 @@ require 'srpl/matchs_container'
 module SRPL
 
   class League
-    
+
     include MatchsContainer
-    
+
+    attr_accessor :title, :game
     attr_reader :players, :matchs, :rounds
-    
+
     def initialize(*players)
       @players = players
       @matchs = []
       @rounds = []
       generate_matchs
     end
-    
+
     def add_player(player)
       @players << player
       generate_matchs
       @players
     end
-    
+
     def find_player(name)
       @players.select do |player|
         player.name == name.to_s
       end
     end
-    
+
     # Return randomly unplayed matchs for each players
     def round(matches = [])
       round = matches.to_a
       players = []
       round.each { |m| players << m.player_1 << m.player_2 }
-      
+
       @matchs.shuffle.each do |m|
-        unless match_in_rounds?(m) || m.finished? || players.include?(m.player_1) || players.include?(m.player_2)
+        unless match_in_rounds?(m) || players.include?(m.player_1) || players.include?(m.player_2) #|| m.finished?
           round << m
           players << m.player_1
           players << m.player_2
         end
       end
-      
+
       round
     end
-    
+
     # Return copy of players sorted by score
     def rank
 #      rank = @players.sort
 #      rank.each { |player| yield player } if block_given?
       @players.sort
     end
-    
+
     # Refresh players score
     def refresh
       @players.each do |player|
@@ -61,11 +62,11 @@ module SRPL
         desertions = 0
         towards = 0
         againsts = 0
-        
+
         matchs = find_match_by_player(player)
         matchs.each do |match|
           next unless match.finished? # skip unfinished matchs
-          
+
           if match.winner == player
             wins += 1
           else
@@ -75,11 +76,11 @@ module SRPL
               defeats += 1
             end
           end
-          
+
           towards += match.player_1 == player ? match.score_1 : match.score_2
           againsts += match.player_1 == player ? match.score_2 : match.score_1
         end
-        
+
         player.instance_exec(wins, defeats, desertions, towards, againsts) do |w, d, de, t, a|
           @wins = w
           @defeats = d
@@ -98,9 +99,9 @@ module SRPL
         super
       end
     end
-    
+
     private
-    
+
     # Génère autant de matchs qu'il y a de possibilités de rencontres entre chaque joueurs
     def generate_matchs
       @players.each do |p1|
@@ -110,14 +111,14 @@ module SRPL
         end
       end
     end
-    
+
     def match_in_rounds?(match)
       @rounds.each do |round|
         return true if round.include? match
       end
       false
     end
-    
+
   end
-  
+
 end
--- lib/srpl/player.rb
+++ lib/srpl/player.rb
@@ -3,9 +3,9 @@ require 'srpl/rules'
 module SRPL
 
   class Player
-  
+
     include Comparable
-    
+
     attr_reader :name # String : name of player
     attr_reader :infos # Hash : informations of player
     attr_accessor :character # String : character playing
@@ -23,32 +23,33 @@ module SRPL
       @againsts = points[:againsts] ? points[:againsts].to_i : 0
       @infos = infos
     end
-    
+
     def plays
       @wins + @defeats + @desertions
     end
-    
+
     def score
       @wins * WIN_POINTS + @defeats * DEFEAT_POINTS + @desertions * DESERTION_POINTS + @opponent_desertions * OPPONENT_DESERTION_POINTS
     end
-    
+
     def goal_average
       @towards - @againsts
     end
-    
+
     # Type can be win, defeat, desertion, opponent_desertion
     def add(type = 'win')
       instance_variable_set("@#{type}s", instance_variable_get("@#{type}s") + 1)
     end
-    
+
     def method_missing(method_name, *args, &block)
+      # add the ability to use dynamic methods like add_<type> (ex: add_win)
       if(/^add_(?<type>\w+)$/ =~ method_name)
         send("add", type)
       else
         super
       end
     end
-    
+
     def ==(another_player)
       if another_player.is_a? Player
         @name == another_player.name
@@ -56,22 +57,25 @@ module SRPL
         false
       end
     end
-    
+
     def <=>(another_player)
+      # sorting by score
       score_ordering = another_player.score <=> score
+      # sorting by goal average if same score
       if score_ordering == 0
         score_ordering = another_player.goal_average <=> goal_average
       end
+      # sorting by name if score and goal average same
       if score_ordering == 0
         score_ordering = @name <=> another_player.name
       end
       score_ordering
     end
-    
+
     def to_s
       @name.to_s
     end
-    
+
   end
 
 end
